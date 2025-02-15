02e0ada9c7f54766d0e600091127a9ed93ada1bf
amineabdalkhalki <amineabdalkhalki@gmail.com>
2025-02-16 00:06:47 +0100

    [CLEAN] remove all the uncessary files

--- backend/ImageProcessorApi/Service/IImageStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿using ImageProcessorApi.Models;
-namespace ImageProcessorApi.Services;
-
-public interface IImageStorage
-{
-    ImageEvent? LatestImage { get; }
-    int LastHourCount { get; }
-    void Update(ImageEvent imageEvent);
-}
