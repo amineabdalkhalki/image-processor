6bd1d6e5f1cac2085038de3f81aa2e71b9d192e5
amineabdalkhalki <amineabdalkhalki@gmail.com>
2025-02-16 00:01:55 +0100

    [FIX] - add baseHref - add "Try again" link - update URL for production

--- frontend/image-ui/src/environments/environment.prod.ts
+++ frontend/image-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:5001/api/images',
+  apiUrl: 'https://7nz6jxmvf4.execute-api.eu-west-3.amazonaws.com/Prod/api/images',
   refreshInterval: 5000, // 5 seconds
   countdownDuration: 5 // seconds
 };
